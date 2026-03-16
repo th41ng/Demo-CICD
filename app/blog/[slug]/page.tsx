5e29429b937241e5431644cf4a58567be86f61c1
th41ng <nguyenlethanhthang@gmail.com>
2026-03-16 20:46:42 +0700

    fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,14 +4,24 @@ type Params = {
   };
 };
 
+export async function generateStaticParams() {
+  return [
+    { slug: "post1" },
+    { slug: "post2" },
+    { slug: "post3" }
+  ];
+}
+
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
 export default function Page({ params }: Params) {
-  return <>
-  <h1>Slug: {params.slug}</h1>
-  <p>ThangMicro</p>
-  <p>HongDi</p>
-</>
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>ThangMicro</p>
+      <p>HongDi</p>
+    </>
+  );
 }
