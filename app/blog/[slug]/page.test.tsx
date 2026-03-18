d80473f7e10fb7983d5ccbd5be5ec924ab26af0f
th41ng <nguyenlethanhthang@gmail.com>
2026-03-18 13:26:13 +0700

    test fail

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -12,5 +12,6 @@ it("App Router: Works with dynamic route segments", () => {
 
 it("ThangMicro should be in this document", () => {
   render(<Page params={{ slug: "Test" }} />);
-  expect(screen.getByText("ThangMicro")).toBeInTheDocument();
+  //expect(screen.getByText("Thang Micro")).toBeInTheDocument();
+  expect(screen.getByText("DuyMicro")).toBeInTheDocument();
 });
